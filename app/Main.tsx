a3890468d429f7fd820b42f9b70c1a553a712fb9
eastend-street <suminari.yamada@gmail.com>
2023-01-21 01:58:07 -0800

    feat: install prettier-plugin-tailwindcss and format

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,6 +1,6 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className="bg-ivory h-screen flex justify-center p-3">
+    <main className="flex h-screen justify-center bg-ivory p-3">
       {children}
     </main>
   );
