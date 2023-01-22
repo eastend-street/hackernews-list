da1086533fc8b77efa902e5362d892ec4322714e
eastend-street <suminari.yamada@gmail.com>
2023-01-21 20:07:49 -0800

    feat: add fetch functions for topstories and each story

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
-export default function Home() {
-  return <div>Main Content</div>;
+const HACKER_NEWS_API_URL = 'https://hacker-news.firebaseio.com';
+
+interface Story {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: 'story';
+  url: string;
+}
+
+const fetchTopStoryIds = async (): Promise<number[]> => {
+  const response = await fetch(`${HACKER_NEWS_API_URL}/v0/topstories.json`);
+  return response.json();
+};
+
+const fetchStoryById = async (storyId: number): Promise<Story> => {
+  const response = await fetch(
+    `${HACKER_NEWS_API_URL}/v0/item/${storyId}.json`
+  );
+  return response.json();
+};
+
+const fetchTopStories = async (): Promise<Story[]> => {
+  const topStoryIds = await fetchTopStoryIds();
+  const topStories = await Promise.all(
+    topStoryIds.map((storyId) => fetchStoryById(storyId))
+  );
+
+  return topStories;
+};
+
+export default async function Home() {
+  const topStories = await fetchTopStories();
+  return (
+    <ul>
+      {topStories.map((story, index) => (
+        <li key={story.id}>
+          {index + 1}. {story.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
