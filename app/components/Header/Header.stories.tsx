d49a1894043d57dedbb3d9c010d4dfb388b469dc
eastend-street <suminari.yamada@gmail.com>
2023-01-29 15:47:47 -0800

    feat: add a story for Header

---
+++ app/components/Header/Header.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './Header';
+
+export default {
+  title: 'components/Header',
+  component: Header,
+};
+
+export const Template = () => <Header />;
+
+export const Default = Template.bind({});
