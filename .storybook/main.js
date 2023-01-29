10e30c9e0bee58af976f05fbeb607d0c7f1e638d
eastend-street <suminari.yamada@gmail.com>
2023-01-29 15:47:31 -0800

    feat: install storybook with Tailwind import

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  stories: ['../app/**/*.stories.@(jsx|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
