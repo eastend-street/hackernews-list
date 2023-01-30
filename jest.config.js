1283d64014d1455ad7450a1fb48eb16ce8b97148
eastend-street <suminari.yamada@gmail.com>
2023-01-29 18:36:35 -0800

    fix: add @/ alias to jest

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
+    '@/(.*)$': '<rootDir>/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
